3abf109853de4a1474d722ae9cd11af80577c0ad
Hayford Iduoriyekemwen <hayfiz@yahoo.com>
2021-05-12 01:08:17 +0100

    updated .listen to listen for dynamically assigned port better

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const https = require ('https');
 const app = express()
 app.use(bodyParser.json());
 
-const localPort = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(process.env.PORT || localPort, () => {
+app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
