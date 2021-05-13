81c52b2d042544a453a2bee98bc8af2428bf2154
Hayford Iduoriyekemwen <hayfiz@yahoo.com>
2021-05-13 08:16:53 +0100

    twitter webhook test 0.21

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ userActivityWebhook.subscribe({
     .on ('tweet_delete', (data) => console.log (userActivity.id + ' - tweet_delete'))
 }).catch(err => {
   console.log('err');
-  console.log(err.body.error);
+  console.log(err.body);
 });
 
 // listen to any user activity
