193f337e102a4236a0f3a1ff6002e26a0fc30020
Hayford Iduoriyekemwen <hayfiz@yahoo.com>
2021-06-14 23:53:04 +0100

    webhook update 0.56

--- server.js
+++ server.js
@@ -3,28 +3,129 @@ const bodyParser = require ('body-parser');
 const twitterWebhooks = require('twitter-webhooks');
 const https = require ('https');
 
+const OAuth = require('oauth')
+const { promisify } = require('util')
+
 const app = express()
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000
 
+var Twit = require('twit')
+
+var T = new Twit({
+  consumer_key:         process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
+  access_token:         process.env.TWITTER_ACCESS_KEY,
+  access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+})
+
+// const stream = T.stream("user");
+// console.log("User stream started 🚀🚀🚀");
+//
+// stream.on("follow", console.log("followed"));
+// stream.on('user_event', function (eventMsg) {
+//   console.log(eventMsg)
+// });
+
+
+// T.get('followers/ids', { screen_name: 'hayfiz' },  function (err, data, response) {
+//   console.log(data)
+// })
+
+
+/** Code to send a dm*/
+// const obj = {
+//       event: {
+//         type: 'message_create',
+//         message_create: {
+//           target: {
+//             recipient_id: 796702896,
+//           },
+//           message_data: {
+//             text: `Hi! 👋`,
+//           },
+//         },
+//       },
+//     };
+//
+// T.post("direct_messages/events/new", obj)
+//         .catch(err => {
+//           console.error("error", err.stack);
+//         })
+//         .then(result => {
+//           console.log(`Message sent successfully To  hype_central  💪💪`);
+//         });
+/***/
+
+
+    // }, timeout);
+
+// getTwitterUserProfileWithOAuth1('hype_central')
+//   .then((profile) => console.log('oauth1 response', JSON.stringify(profile, null, 2)) && process.exit(0))
+//   .catch(err => console.error(err) && process.exit(1))
+//
+// async function getTwitterUserProfileWithOAuth1 (username = 'hype_central') {
+//   var oauth = new OAuth.OAuth(
+//     'https://api.twitter.com/oauth/request_token',
+//     'https://api.twitter.com/oauth/access_token',
+//     process.env.TWITTER_CONSUMER_KEY,
+//     process.env.TWITTER_CONSUMER_SECRET,
+//     '1.0A', null, 'HMAC-SHA1'
+//   )
+//   // const get = promisify(oauth.get.bind(oauth))
+//   //
+//   // const body = await get(
+//   //   `https://api.twitter.com/1.1/users/show.json?screen_name=${username}`,
+//   //   process.env.TWITTER_ACCESS_KEY,
+//   //   process.env.TWITTER_ACCESS_TOKEN_SECRET
+//   // )
+//
+//   var request_body = {
+//         event: {
+//           type: 'message_create',
+//           message_create: {
+//             target: {
+//               recipient_id: 796702896,
+//             },
+//             message_data: {
+//               text: `Hi! 👋`,
+//             },
+//           },
+//         },
+//       };
+//
+//   const post = promisify(oauth.post.bind(oauth))
+//
+//   const body = await post(
+//     'https://api.twitter.com/1.1/direct_messages/events/new.json',
+//     process.env.TWITTER_ACCESS_KEY,
+//     process.env.TWITTER_ACCESS_TOKEN_SECRET,
+//     JSON.stringify(request_body)
+//   )
+//
+//   return JSON.parse(body)
+// }
+
 const userActivityWebhook = twitterWebhooks.userActivity({
     serverUrl: 'https://whats-down-this-hole.herokuapp.com/',
     route: '/', //default : '/'
-    consumerKey: 'PtQ6Alt8ZW4c2Y48sad6cZkKJ',
-    consumerSecret: 'JDY0G3McBTKdyl6B6LtmpEN5vAxpgpIq7qp6k5shuYBvGn7YFY',
-    accessToken: '316270387-F1jRV5VeBoWkcz1fTyQCRnxEZErvgsJ2TSrER6cm',
-    accessTokenSecret: 'tvl0LHycdYl3SWbkpFSHqPCsSXxmsOZ10ccq36nR5fuuA',
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_KEY,
+    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
     environment: 'development', //default : 'env-beta'
     appBearerToken: 'AAAAAAAAAAAAAAAAAAAAAM%2FsPQEAAAAAYa16T%2BFIkXX9fdO9xYdUBVX1wi8%3DY2ialfV498fCONhbWsTW4bo4gaWuJnkL7eZq5CRpjfOFhWVmS5',
     app
 });
 
 //Register your webhook url - just needed once per URL
-// userActivityWebhook.register().catch(err => {
-//   console.log('err on register');
-//   console.log(err);
-// });;
+userActivityWebhook.register().catch(err => {
+  console.log('err on register');
+  console.log(err);
+});;
 
 // Subscribe for a particular user activity
 userActivityWebhook.subscribe({
@@ -74,7 +175,7 @@ userActivityWebhook.subscribe({
 // });
 
 // listen to any user activity
-userActivityWebhook.on ('event', (event, userId, data) => console.log (userId + ' - favorite'));
+// userActivityWebhook.on ('event', (event, userId, data) => console.log (userId + ' - favorite'));
 
 // listen to unknown payload (in case of api new features)
 // userActivityWebhook.on ('unknown-event', (rawData) => console.log (rawData));
