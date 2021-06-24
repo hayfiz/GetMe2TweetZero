668a2156b4b7e47c35d447ae73c453418c7596cb
Hayford Iduoriyekemwen <hayfiz@yahoo.com>
2021-06-24 23:52:49 +0100

    webhook test 0.75

--- server.js
+++ server.js
@@ -140,19 +140,25 @@ function digTweet(data, recipient_id) {
 //   console.log(data)
 // })
 
-var request = require('request');
-var options = {
-  'method': 'GET',
-  'url': 'https://api.twitter.com/2/tweets/1408182889578217475?tweet.fields=referenced_tweets',
-  'headers': {
-    'Authorization': 'OAuth oauth_consumer_key="PtQ6Alt8ZW4c2Y48sad6cZkKJ",oauth_token="316270387-F1jRV5VeBoWkcz1fTyQCRnxEZErvgsJ2TSrER6cm",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1624572624",oauth_nonce="gUhLtE8M0yN",oauth_version="1.0",oauth_signature="1pFi3C%2Ftk42iKBv7WrfzkAp01Eg%3D"',
+var axios = require('axios');
+
+var config = {
+  method: 'get',
+  url: 'https://api.twitter.com/2/tweets/1408182889578217475?tweet.fields=referenced_tweets',
+  headers: {
+    'Authorization': 'OAuth oauth_consumer_key="PtQ6Alt8ZW4c2Y48sad6cZkKJ",oauth_token="316270387-F1jRV5VeBoWkcz1fTyQCRnxEZErvgsJ2TSrER6cm",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1624575136",oauth_nonce="GtC9Se54dmt",oauth_version="1.0",oauth_signature="tkkZrtFxxSVEuVn1j%2Bt7loXhV%2BI%3D"',
     'Cookie': 'guest_id=v1%3A162088818648439981; personalization_id="v1_x0G0pyVUfxh6NfBACXkQ7w=="'
   }
 };
-request(options, function (error, response) {
-  if (error) throw new Error(error);
-  console.log(response.body);
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
 });
+
 
 
 
