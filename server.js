3c1ceb094245248d8a0d990c88d66ddcc8386a7a
Hayford Iduoriyekemwen <hayfiz@yahoo.com>
2021-07-29 22:42:08 +0100

    webhook test 0.123

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const bodyParser = require ('body-parser');
+const bodyParser = require('body-parser');
 const twitterWebhooks = require('twitter-webhooks');
-const https = require ('https');
 const { TwitterApi } = require('twitter-api-v2');
-
-const { promisify } = require('util')
 
 const app = express()
 app.use(bodyParser.json());
@@ -23,38 +20,38 @@ const client = new TwitterApi({
 });
 
 const userActivityWebhook = twitterWebhooks.userActivity({
-    serverUrl: 'https://whats-down-this-hole.herokuapp.com/',
-    route: '/', //default : '/'
-    consumerKey: process.env.TWITTER_CONSUMER_KEY,
-    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-    accessToken: process.env.TWITTER_ACCESS_KEY,
-    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-    environment: 'development', //default : 'env-beta'
-    appBearerToken: 'AAAAAAAAAAAAAAAAAAAAAM%2FsPQEAAAAAYa16T%2BFIkXX9fdO9xYdUBVX1wi8%3DY2ialfV498fCONhbWsTW4bo4gaWuJnkL7eZq5CRpjfOFhWVmS5', //// TODO: Move bearer token to env variable
-    app
+  serverUrl: 'https://whats-down-this-hole.herokuapp.com/',
+  route: '/', // default : '/'
+  consumerKey: process.env.TWITTER_CONSUMER_KEY,
+  consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_KEY,
+  accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  environment: 'development', // default : 'env-beta'
+  appBearerToken: 'AAAAAAAAAAAAAAAAAAAAAM%2FsPQEAAAAAYa16T%2BFIkXX9fdO9xYdUBVX1wi8%3DY2ialfV498fCONhbWsTW4bo4gaWuJnkL7eZq5CRpjfOFhWVmS5', /// / TODO: Move bearer token to env variable
+  app
 });
 
-var Twit = require('twit')
+const Twit = require('twit')
 
-var T = new Twit({
-  consumer_key:         process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
-  access_token:         process.env.TWITTER_ACCESS_KEY,
-  access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+const T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+  strictSSL: true, // optional - requires SSL certificates to be valid.
 })
 
-//// TODO:  Combine methods below to register webhook if none is registered
-/**
+/// / TODO:  Combine methods below to register webhook if none is registered
 
-//Register your webhook url - just needed once per URL
-userActivityWebhook.register().catch(err => {
+
+// Register your webhook url - just needed once per URL
+userActivityWebhook.register().catch((err) => {
   console.log('err on register');
   console.log(err);
 });;
 
-*/
+
 
 // Get webhook info
 // userActivityWebhook.getWebhook()
@@ -65,41 +62,41 @@ userActivityWebhook.register().catch(err => {
 //   console.log(err.body);
 // });
 
-if (process.env.TWITTER_BOT_ACTIVE === "Y") {
+if (process.env.TWITTER_BOT_ACTIVE === 'Y') {
   // Unsubscribe for a particular user activity
   userActivityWebhook.unsubscribe({
-      userId: '316270387',
-      accessToken: process.env.TWITTER_ACCESS_KEY,
-      accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    userId: '316270387',
+    accessToken: process.env.TWITTER_ACCESS_KEY,
+    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
   })
-  .then(function (ret) {
+    .then(() => {
       console.log('Removed previous subscription for user activity ❌')
       // Subscribe for a particular user activity
       userActivityWebhook.subscribe({
-          userId: '316270387',
-          accessToken: process.env.TWITTER_ACCESS_KEY,
-          accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+        userId: '316270387',
+        accessToken: process.env.TWITTER_ACCESS_KEY,
+        accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
       })
-      .then(function (userActivity) {
+        .then((userActivity) => {
           userActivity
-          .on ('tweet_create', (data) => {
-                if (data.in_reply_to_status_id) {
-                  digTweet(data.user.screen_name, data.in_reply_to_status_id_str, data.user.id);
-                } else {
-                  console.log(`${data.id_str}: A tweet was created but it's being ignored since it is not a mention`);
-                }
-              })
+            .on ('tweet_create', (data) => {
+              if (data.in_reply_to_status_id) {
+                digTweet(data.user.screen_name, data.in_reply_to_status_id_str, data.user.id);
+              } else {
+                console.log(`${data.id_str}: A tweet was created but it's being ignored since it is not a mention`);
+              }
             })
-            .then(function (ret) {
-              console.log('Successfully subscribed to user activity ✅');
-            }).catch(err => {
-              console.log('err on subscribe 🤮');
-              console.log(err.body);
-            });
-  }).catch(err => {
-    console.log('err on unsubscribe 🤮');
-    console.log(err.body);
-  });
+        })
+        .then(() => {
+          console.log('Successfully subscribed to user activity ✅');
+        }).catch((err) => {
+          console.log('err on subscribe 🤮');
+          console.log(err.body);
+        });
+    }).catch((err) => {
+      console.log('err on unsubscribe 🤮');
+      console.log(err.body);
+    });
 
 }
 
@@ -111,13 +108,13 @@ if (process.env.TWITTER_BOT_ACTIVE === "Y") {
 */
 async function digTweet(authorUserName, tweetId, recipientId) {
   if (tweetId) {
-  //search for referenced tweets
+  // search for referenced tweets
     await searchForTweet(tweetId).then(async(value) => {
       if (value && value.data.referenced_tweets) {
         var authorUserName = value.includes.users[0].username;
         var tweetId = value.data.referenced_tweets[0].id;
 
-        //call digTweet on referenced tweets
+        // call digTweet on referenced tweets
         await digTweet(authorUserName, tweetId, recipientId);
       }
     });
